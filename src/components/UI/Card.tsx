17c57c23337378ad0d877882cc95a84034f20d82
Viunze <cellodestian5@gmail.com>
2025-11-27 10:38:45 +0700

    Create Card.tsx

---
+++ src/components/UI/Card.tsx
@@ -0,0 +1,20 @@
+// src/components/UI/Card.tsx
+
+import React from 'react';
+
+interface CardProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Card: React.FC<CardProps> = ({ children, className }) => {
+  return (
+    <div
+      className={`bg-white rounded-xl shadow-jawa-soft p-6 transition-all duration-300 ${className || ''}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
